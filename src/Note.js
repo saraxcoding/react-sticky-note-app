bae96ef722c01bb475851645c728c8d4936958e5
Sara H <80139090+saraxcoding@users.noreply.github.com>
2022-02-10 01:40:49 -0500

    Update Note.js

--- src/Note.js
+++ src/Note.js
@@ -11,29 +11,27 @@ const Note = (props) => {
     const editMeId = props.note.id;
     props.onType(editMeId, "description", updatedValue);
   };
-  const clickDelete = () => props.remove(props.note.id);
+  const clickDelete = () => props.removeNote(props.note.id);
 
   return (
-    <div>
-      <li className="note">
-        <input
-          type="text"
-          value={props.note.title}
-          onChange={updateTitle}
-          placeholder="Title"
-          className="note__title"
-        />
-        <textarea
-          value={props.note.description}
-          onChange={updateDescription}
-          placeholder="Description..."
-          className="note__description"
-        />
-        <span onClick={clickDelete} className="note__delete">
-          X
-        </span>
-      </li>
-    </div>
+     <li className="note">
+       <input
+         type="text"
+         value={props.note.title}
+         onChange={updateTitle}
+         placeholder="Title"
+         className="note__title"
+       />
+       <textarea
+         value={props.note.description}
+         onChange={updateDescription}
+         placeholder="Description..."
+         className="note__description"
+       />
+       <span onClick={clickDelete} className="note__delete">
+         X
+       </span>
+     </li>
   );
 };
 
